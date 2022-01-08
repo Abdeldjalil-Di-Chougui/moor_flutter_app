b010da2bebcf1d9c4b1a2085548c126cae6f7773
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-01-08 12:42:36 +0100

    commit home page ui updates

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+import '../data/moor_database.dart';
+import 'widget/new_tag_input_widget.dart';
+import 'widget/new_task_input_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Tasks'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(child: _buildTaskList(context)),
+            NewTaskInput(),
+            NewTagInput(),
+          ],
+        ));
+  }
+
+  StreamBuilder<List<TaskWithTag>> _buildTaskList(BuildContext context) {
+    final dao = Provider.of<TaskDao>(context);
+    return StreamBuilder(
+      stream: dao.watchAllTasks(),
+      builder: (context, AsyncSnapshot<List<TaskWithTag>> snapshot) {
+        final tasks = snapshot.data ?? List();
+
+        return ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (_, index) {
+            final item = tasks[index];
+            return _buildListItem(item, dao);
+          },
+        );
+      },
+    );
+  }
+
+}

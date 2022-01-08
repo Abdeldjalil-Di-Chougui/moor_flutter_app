9a5dca6a41ccc46009f2af195ea8a83b07302a5a
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-01-08 09:37:53 +0100

    commit moor database dao implementation

--- lib/moor_database.dart
+++ lib/moor_database.dart
@@ -9,17 +9,35 @@ class Tasks extends Table {
   IntColumn get category => integer().nullable()();
 }
 
-@UseMoor(tables: [Tasks])
+@UseMoor(tables: [Tasks], daos: [TaskDao])
 class MyDatabase extends _$MyDatabase  {
   MyDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite', logStatements: true));
 
   @override
   int get schemaVersion => 2;
+}
+
+@UseDao(
+  tables: [Tasks],
+  queries: {
+    'watchCategoryOneTasks': 'SELECT * FROM Tasks WHERE category = 1 ORDER BY title ASC;'
+  }
+)
+class TaskDao extends DatabaseAccessor<MyDatabase> with _$TaskDaoMixin {
+
+  final MyDatabase db;
+  TaskDao(this.db) : super(db);
 
   Future<List<Task>> getAllTasks() => select(tasks).get();
   Stream<List<Task>> watchAllTasks() => select(tasks).watch();
+  // Stream<List<Task>> watchCategoryOneTasks() => (select(tasks)
+  //     ..orderBy([
+  //         (t) => OrderingTerm(expression: t.title, mode: OrderingMode.asc),
+  //     ])
+  //     ..where((t) => t.category.equals(1)))
+  //     .watch();
 
-  Future<int> insertTask(task) => into(tasks).insert(task);
-  Future updateTask(task) => update(tasks).replace(task);
-  Future deleteTask(task) => delete(tasks).delete(task);
+  Future<int> insertTask(Insertable<Task> task) => into(tasks).insert(task);
+  Future updateTask(Insertable<Task> task) => update(tasks).replace(task);
+  Future deleteTask(Insertable<Task> task) => delete(tasks).delete(task);
 }

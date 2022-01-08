f98fce8562267bb22684be8a70ba64e04fea73b5
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-01-08 02:00:23 +0100

    commit moore database update queries

--- lib/moor_database.dart
+++ lib/moor_database.dart
@@ -2,18 +2,19 @@ import 'package:moor_flutter/moor_flutter.dart';
 
 part 'moor_database.g.dart';
 
-class Todo extends Table {
+class Tasks extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 3, max: 25)();
   TextColumn get content => text().named('body')();
   IntColumn get category => integer().nullable()();
 }
 
-@UseMoor(tables: [Todo])
+@UseMoor(tables: [Tasks])
 class MyDatabase extends _$MyDatabase  {
   MyDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite', logStatements: true));
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
 
+  Future<List<Task>> getAllTasks() => select(tasks).get();
 }

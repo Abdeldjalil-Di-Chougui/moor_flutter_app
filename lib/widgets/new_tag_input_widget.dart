8da604e4f5d795d260f526435aff89f8dd89b6fe
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-01-08 12:45:56 +0100

    commit new tag input widget

---
+++ lib/widgets/new_tag_input_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+import 'package:moor/moor.dart';
+import 'package:provider/provider.dart';
+
+class NewTagInput extends StatefulWidget {
+  const NewTagInput({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _NewTagInputState createState() => _NewTagInputState();
+}
+
+class _NewTagInputState extends State<NewTagInput> {
+  static const Color DEFAULT_COLOR = Colors.red;
+
+  Color pickedTagColor = DEFAULT_COLOR;
+  TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          _buildTextField(context),
+          _buildColorPickerButton(context),
+        ],
+      ),
+    );
+  }
+}

f53d64a2007e67c81fa30bda70b00b90b52bddb3
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-01-08 13:01:32 +0100

    commit moor db updates

--- lib/moor_database.dart
+++ lib/moor_database.dart
@@ -20,6 +20,16 @@ class Tags extends Table {
   Set<Column> get primaryKey => {name};
 }
 
+class TaskWithTag {
+  final Task task;
+  final Tag tag;
+
+  TaskWithTag({
+    @required this.task,
+    @required this.tag,
+  });
+}
+
 @UseMoor(tables: [Tasks, Tags], daos: [TaskDao])
 class MyDatabase extends _$MyDatabase  {
   MyDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'db.sqlite', logStatements: true));
@@ -53,13 +63,38 @@ class TaskDao extends DatabaseAccessor<MyDatabase> with _$TaskDaoMixin {
   TaskDao(this.db) : super(db);
 
   Future<List<Task>> getAllTasks() => select(tasks).get();
-  Stream<List<Task>> watchAllTasks() => select(tasks).watch();
+  // Stream<List<Task>> watchAllTasks() => select(tasks).watch();
   // Stream<List<Task>> getCategoryOneTasks() => (select(tasks)
   //     ..orderBy([
   //         (t) => OrderingTerm(expression: t.title, mode: OrderingMode.asc),
   //     ])
   //     ..where((t) => t.category.equals(1)))
   //     .watch();
+
+  Stream<List<TaskWithTag>> watchAllTasks() {
+    return (select(tasks)
+      ..orderBy(
+        [
+              (t) =>
+              OrderingTerm(expression: t.dueDate, mode: OrderingMode.desc),
+              (t) => OrderingTerm(expression: t.name),
+        ],
+      ))
+        .join(
+      [
+        leftOuterJoin(tags, tags.name.equalsExp(tasks.tagName)),
+      ],
+    )
+        .watch()
+        .map((rows) => rows.map(
+          (row) {
+        return TaskWithTag(
+          task: row.readTable(tasks),
+          tag: row.readTable(tags),
+        );
+      },
+    ).toList());
+  }
 
   Future<int> insertTask(Insertable<Task> task) => into(tasks).insert(task);
   Future updateTask(Insertable<Task> task) => update(tasks).replace(task);
